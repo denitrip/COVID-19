7e6bba25743824a18dc20d6ba43ddc4b8167e1ae
SpaNb4 <spanb4@gmail.com>
2020-12-17 19:20:04 +0300

    feat: add updatedata functional

--- src/js/global_stats.js
+++ src/js/global_stats.js
@@ -11,12 +11,14 @@ const totalStatsHeading = '.total_deaths';
 const USstatsUl = '.us_deaths_list ul';
 
 let countriesArr = [];
+let currCountry;
 countriesArr[0] = { value: 'Global' };
 
 // selected country click
-export const countriesSelect = document.querySelector('#countries');
+const countriesSelect = document.querySelector('#countries');
 countriesSelect.addEventListener('change', () => {
-    updateData();
+    currCountry = countriesSelect.childNodes[0].value;
+    updateData(currCountry);
 });
 
 let tabButtons = document.querySelectorAll('.categories .tablinks');
@@ -25,7 +27,7 @@ tabButtons.forEach((item) => {
         // remove "_btn" from id's
         let correctId = item.id.substring(0, item.id.indexOf('_btn'));
         showTabContent(event, correctId);
-        updateData();
+        updateData(currCountry);
     });
 });
 
@@ -36,37 +38,31 @@ let thousandBtn = document.querySelector('#per_100_thousand_btn');
 thousandBtn.addEventListener('click', () => {
     thousandBtn.classList.add('active');
     absoluteBtn.classList.remove('active');
-    updateData();
+    updateData(currCountry);
 });
 
 let absoluteBtn = document.querySelector('#absolute_btn');
 absoluteBtn.addEventListener('click', () => {
     absoluteBtn.classList.add('active');
     thousandBtn.classList.remove('active');
-    updateData();
+    updateData(currCountry);
 });
 
 let allBtn = document.querySelector('#all_btn');
 allBtn.addEventListener('click', () => {
     allBtn.classList.add('active');
     lastDayBtn.classList.remove('active');
-    updateData();
+    updateData(currCountry);
 });
 
 let lastDayBtn = document.querySelector('#last_day_btn');
 lastDayBtn.addEventListener('click', () => {
     lastDayBtn.classList.add('active');
     allBtn.classList.remove('active');
-    updateData();
+    updateData(currCountry);
 });
 
 function showTabContent(e, field) {
-    //hide all elements with "tabcontent" class
-    let tabcontent = document.querySelectorAll('.tabcontent');
-    tabcontent.forEach((item) => {
-        item.style.display = 'none';
-    });
-
     // remove "active" class from all elements
     let tablinks = document.querySelectorAll('.categories .tablinks');
     tablinks.forEach((item) => {
@@ -81,86 +77,107 @@ function showTabContent(e, field) {
             item.classList.add('active');
         }
     });
+}
 
-    let currContent = document.querySelectorAll(`#${field}`);
-    // show the current tab, and add an "active" class
-    currContent.forEach((item) => {
-        item.style.display = 'block';
+export function checkField() {
+    let tablinks = document.querySelectorAll('.categories .tablinks');
+    let field;
+    tablinks.forEach((item) => {
+        if (item.classList.contains('active')) {
+            field = item.id.substring(0, item.id.indexOf('_btn'));
+        }
     });
-}
 
-function checkField(field, item, isAll, isAbsolute) {
-    let value;
-    let allDays;
-    let lastDay;
+    let isAllBtns = document.querySelectorAll('.isAll .tablinks');
+    let isAll;
+    isAllBtns.forEach((item) => {
+        if (item.classList.contains('active')) {
+            if (item.id === 'all_btn') {
+                isAll = true;
+            } else {
+                isAll = false;
+            }
+        }
+    });
 
-    if (field == 'deaths') {
-        allDays = item['deaths'];
-        lastDay = item['todayDeaths'];
-    } else if (field == 'cases') {
-        allDays = item['cases'];
-        lastDay = item['todayCases'];
-    } else if (field == 'recovered') {
-        allDays = item['recovered'];
-        lastDay = item['todayRecovered'];
-    }
-
-    // all days
+    let res = {};
     if (isAll) {
-        if (isAbsolute) {
-            value = allDays;
-        } else {
-            if (item.population) {
-                value = (allDays / item.population) * 100000;
-            }
-        }
-        // only last day
+        res.field = field;
     } else {
-        if (isAbsolute) {
-            value = lastDay;
-        } else {
-            if (item.population) {
-                value = (lastDay / item.population) * 100000;
-            }
-        }
+        res.field = 'today' + field[0].toUpperCase() + field.slice(1);
     }
+    res.isAll = isAll;
 
-    if (!value) {
-        value = 0;
-    }
-
-    return value;
+    return res;
 }
 
-export function setGlobalStats(url, field, isAbsolute, isAll, country) {
+export function sortArr(result) {
+    let isAbsoluteBtns = document.querySelectorAll('.isAbsolute .tablinks');
+    let isAbsolute;
+    isAbsoluteBtns.forEach((item) => {
+        if (item.classList.contains('active')) {
+            if (item.id === 'absolute_btn') {
+                isAbsolute = true;
+            } else {
+                isAbsolute = false;
+            }
+        }
+    });
+
+    let field = checkField().field;
+
+    if (isAbsolute) {
+        result.sort((a, b) => {
+            return b[`${field}`] - a[`${field}`];
+        });
+    } else {
+        for (let i = 0; i < result.length; i += 1) {
+            result[i][`${field}`] = (result[i][`${field}`] / result[i]['population']) * 100000;
+            if (!result[i][`${field}`] || result[i][`${field}`] == Infinity) {
+                result[i][`${field}`] = 0;
+            }
+        }
+        result.sort((a, b) => {
+            if (a[`${field}`] && b[`${field}`]) {
+                return b[`${field}`] - a[`${field}`];
+            }
+        });
+    }
+}
+
+export function setGlobalStats(url, country) {
     fetch(url)
         .then((response) => response.json())
         .then((result) => {
             let sum = 0;
-            result.sort((a, b) => {
-                return b[`${field}`] - a[`${field}`];
-            });
 
-            const globalStats = document.querySelector(`#${field} ${totalStatsUl}`);
-            const globalStatsList = document.querySelector(`#${field} ${totalStatsUlList}`);
+            const globalStats = document.querySelector(`${totalStatsUl}`);
+            const globalStatsList = document.querySelector(`${totalStatsUlList}`);
             globalStats.innerHTML = '';
             globalStatsList.innerHTML = '';
 
-            result.forEach((item, index) => {
-                let value = checkField(field, item, isAll, isAbsolute);
+            sortArr(result);
+            let field = checkField().field;
 
+            result.forEach((item, index) => {
+                let value = item[`${field}`];
+                if (!value) {
+                    value = 0;
+                }
                 sum += value;
+
                 let itemLi = document.createElement('li');
                 let itemLiList = document.createElement('li');
                 itemLi.innerHTML = `<span>
-                                        <span class='stats_value'>${value.toLocaleString()}</span> ${field}
+                                        <span class='stats_value'>${value.toLocaleString()}</span> ${field.replace('today', '')}
                                     </span>
                                     <span class='stats_value'>${item.country}</span>`;
                 itemLiList.innerHTML = `<span class="list_item">
-                                    <span class='stats_value'>${value.toLocaleString()}</span> <span class="list_country">${item.country}</span> <img src=${item.countryInfo.flag} alt='flag'>
-                                </span>`;
+                                            <span class='stats_value stats_value_red'>${value.toLocaleString()}</span> <span class="list_country">${item.country}</span> 
+                                            <img src=${item.countryInfo.flag} alt='flag'>
+                                        </span>`;
                 // global stats
-                if (country == 'Global') {
+                if (!country || country=='Global') {
                     globalStats.append(itemLi);
                     globalStatsList.append(itemLiList);
                     // if specific country clicked
@@ -172,7 +189,7 @@ export function setGlobalStats(url, field, isAbsolute, isAll, country) {
                 countriesArr[index + 1] = {};
                 countriesArr[index + 1]['value'] = item.country;
 
-                const totalStats = document.querySelector(`#${field} ${totalStatsHeading}`);
+                const totalStats = document.querySelector(`${totalStatsHeading}`);
                 totalStats.innerHTML = `${sum.toLocaleString()}`;
             });
 
@@ -181,32 +198,37 @@ export function setGlobalStats(url, field, isAbsolute, isAll, country) {
                 silent: true,
                 shouldSort: false,
             });
-            country = countriesSelect.childNodes[0].value;
 
-            const totalStatsList = document.querySelector(`#${field} ${totalStatsUlList}`);
+            const totalStatsList = document.querySelector(`${totalStatsUlList}`);
             totalStatsList.onclick = (e) => {
                 let listItem = e.target.closest('.list_item');
                 if (!listItem) return;
-                let countryName = listItem.querySelector('.list_country').innerHTML;
-                updateData(countryName);
+                currCountry = listItem.querySelector('.list_country').innerHTML;
+                updateData(currCountry);
             };
         });
 }
 
-export function setUSstats(url, field, isAbsolute, isAll) {
+export function setUSstats(url) {
     fetch(url)
         .then((response) => response.json())
         .then((result) => {
             const USstats = document.querySelector(USstatsUl);
             USstats.innerHTML = '';
 
+            sortArr(result);
+            let field = checkField().field;
+
             result.forEach((item) => {
-                let value = checkField(field, item, isAll, isAbsolute);
+                let value = item[`${field}`];
+                if (!value) {
+                    value = 0;
+                }
                 let itemLi = document.createElement('li');
                 itemLi.innerHTML = `<span>
-                                            <span class='stats_value'>${value.toLocaleString()} <span>${field}</span></span>
-                                        </span>
-                                        <span class='stats_value'>${item.state} US</span>`;
+                                        <span class='stats_value'>${value.toLocaleString()} <span>${field.replace('today', '')}</span></span>
+                                    </span>
+                                    <span class='stats_value'>${item.state} US</span>`;
                 USstats.append(itemLi);
             });
         });
