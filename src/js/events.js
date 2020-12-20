891161d67536b4080969497d681f074b54f74fa7
SpaNb4 <spanb4@gmail.com>
2020-12-20 23:20:15 +0300

    feat: add sync with map radiobuttons

--- src/js/events.js
+++ src/js/events.js
@@ -1,6 +1,7 @@
 import { utils } from './map_utils';
 import { state } from './state';
 import { createPointLayer, createGeoJson, selectPointerMove } from './map_layer';
+import { updateData } from './service';
 
 const stats = document.querySelector('#map-stats');
 const statsClose = stats.querySelector('#map-stats-close');
@@ -11,49 +12,55 @@ const statsCountry = stats.querySelector('#map-stats-country');
 const statsCases = stats.querySelector('#map-stats-cases');
 const statsDeaths = stats.querySelector('#map-stats-deaths');
 const statsRecovered = stats.querySelector('#map-stats-recovered');
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input[type=radio]');
 
 const ondRateClick = (evt) => {
-  //  close popups
-  utils.closeStats();
-  utils.closeLegend();
+    //  close popups
+    utils.closeStats();
+    utils.closeLegend();
 
-  //  change type of rate
-  state.rate[evt.target.name] = evt.target.value;
-  // redraw map
-  createPointLayer();
-}
+    //  change type of rate
+    state.rate[evt.target.name] = evt.target.value;
+
+    document.querySelectorAll(`#${evt.target.value}`).forEach((item) => {
+        item.checked = true;
+    });
+    updateData();
+
+    // redraw map
+    createPointLayer();
+};
 
 const onPointSelect = (evt) => {
-  const arr = evt.target.getFeatures().getArray();
-  if (!arr.length) return;
+    const arr = evt.target.getFeatures().getArray();
+    if (!arr.length) return;
 
-  const obj = arr[0].getProperties();
+    const obj = arr[0].getProperties();
 
-  statsCountry.textContent = obj.country;
+    statsCountry.textContent = obj.country;
 
-  if (state.rate.period === 'cumulative') {
-    statsCases.textContent = obj.cases;
-    statsDeaths.textContent = obj.deaths;
-    statsRecovered.textContent = obj.recovered;
-  } else {
-    statsCases.textContent = obj.todayCases;
-    statsDeaths.textContent = obj.todayDeaths;
-    statsRecovered.textContent = obj.todayRecovered;
-  }
+    if (state.rate.period === 'cumulative') {
+        statsCases.textContent = obj.cases;
+        statsDeaths.textContent = obj.deaths;
+        statsRecovered.textContent = obj.recovered;
+    } else {
+        statsCases.textContent = obj.todayCases;
+        statsDeaths.textContent = obj.todayDeaths;
+        statsRecovered.textContent = obj.todayRecovered;
+    }
 
-  utils.openStats(); // open statistics
-}
+    utils.openStats(); // open statistics
+};
 
 window.addEventListener('load', () => {
-  // create geo json for map
-  createGeoJson();
-  // radio toggle event handlers
-  [... inputs].forEach((input) => input.addEventListener('change', ondRateClick));
-  // points event handler
-  selectPointerMove.on('select', onPointSelect);
-  // map popups event handlers
-  statsClose.addEventListener('click', utils.closeStats);
-  legendOpen.addEventListener('click', utils.openLegend);
-  legendClose.addEventListener('click', utils.closeLegend);
+    // create geo json for map
+    createGeoJson();
+    // radio toggle event handlers
+    [...inputs].forEach((input) => input.addEventListener('change', ondRateClick));
+    // points event handler
+    selectPointerMove.on('select', onPointSelect);
+    // map popups event handlers
+    statsClose.addEventListener('click', utils.closeStats);
+    legendOpen.addEventListener('click', utils.openLegend);
+    legendClose.addEventListener('click', utils.closeLegend);
 });
